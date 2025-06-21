905bcc09043bfbba414abcb48fb5d0e045615b11
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-06-22 00:06:38 +0530

    updated links route

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,21 @@
-import Script from "next/script";
 
-export default function Home() {
-
-  
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center text-gray-800">
-
-       <Script
-        strategy="beforeInteractive"
-        defer
-        data-domain="nishul.dev"
-        src="https://plausible.io/js/script.js"
-      />
-
-      <div className="max-w-xl px-6 text-sm leading-relaxed font-light">
-        <h1 className="font-serif text-2xl mb-4">
-          N—D<sup className="text-xs align-super">©</sup>
-        </h1>
-        <p className="font-sans mb-3 border-b border-gray-300 pb-3">
-          Nishul Dhakar is a full-stack developer passionate about building clean interfaces & smart systems. Currently exploring AI-powered tools and digital productivity.
-        </p>
-        <span className=""></span>
-        <p className="font-sans text-gray-500">
-          My 2025 portfolio is under construction — while that’s happening, you can find me on <a href="https://x.com/NishulDhakar" className="underline">twitter </a>, or say hello at <a href="https://t.me/nishuldhakar" className="underline">telegram</a>.
-        </p>
-      </div>
-    </div>
-  );
+export default function HomePage(){
+  return(
+    <>
+      <div className="min-h-screen bg-neutral-950  flex items-center justify-center text-neutral-400">
+    <div className="max-w-xl px-6 text-sm leading-relaxed font-light">
+      <h1 className="font-serif text-2xl mb-4">
+         N—D<sup className="text-xs align-super">©</sup>
+      </h1>
+       <p className="font-sans mb-3 border-b text-neutral-200 pb-3">
+         Nishul Dhakar is a full-stack developer passionate about building clean interfaces & smart systems. Currently exploring AI-powered tools and digital productivity.
+      </p>
+      <span className=""></span>
+      <p className="font-sans text-neutral-100">
+        My 2025 portfolio is under construction — while that’s happening, you can find me <a href="https://nishul.dev/links" target="_blank" className="underline text-white hover:text-gray-400">Links</a>.
+       </p>
+  </div>
+ </div>
+    </>
+  )
 }
