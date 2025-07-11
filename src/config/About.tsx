4e245b48584862dfca97c33b00a2f010f7f3aa11
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-07-12 02:02:34 +0530

    Update About.tsx

--- src/config/About.tsx
+++ src/config/About.tsx
@@ -1,5 +1,5 @@
 export const about = {
   name: "Nishul Dhakar",
   description:
-    "Hey I am Nishul, a 21-year-old full-stack developer currently in my final year of B.Tech in AI/ML. I build modern web applications with Nextjs React, TypeScript, Tailwind, Node.js, and MongoDB. Besides development, I am also skilled in Data Structures & Algorithms and practice daily LeetCode problems with Java",
+    "Hey I am Nishul, a 21-year-old full-stack developer currently in my final year of B.Tech. I build modern web applications with Nextjs React, TypeScript, Tailwind, Node.js, and MongoDB. Besides development, I am also skilled in Data Structures & Algorithms and practice daily LeetCode problems with Java",
 };
