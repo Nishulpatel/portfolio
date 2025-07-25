2f80a68b444460aea1415d6210817ea94a049e8a
nishuldhakar <nishuldhakar123@example.com>
2025-07-26 01:51:08 +0530

    updated certificateData

--- src/data/Certificate.tsx
+++ src/data/Certificate.tsx
@@ -4,13 +4,13 @@
 export const certificateData = [
   {
     title: "Azure Fundamentals",
-    description: "Completed the Azure Fundamentals certification via Coursera in March 2025.",
+    description: "Completed the Azure Fundamentals certification via microsoft  in March 2025.",
     href: "https://www.certiport.com/portal/Pages/PrintTranscriptInfo.aspx?action=Cert&id=414&cvid=OZGa2+qwLG7ppMQJskGPCQ==",
     image: "/Azur.png",
   },
   {
     title: "Web Development",
-    description: "Completed the Web Development course by Amazon in March 2025.",
+    description: "Completed the Web Development course by Udemy in March 2025.",
     href: "https://www.udemy.com/certificate/UC-cf75dbee-50fc-4952-977a-8ff11f432959/",
     image: "/web.jpg",
   },
