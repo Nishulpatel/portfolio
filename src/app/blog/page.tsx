ba59a460df3093db38ccd3c08f066b5f70cacc6d
nishuldhakar <nishuldhakar123@example.com>
2025-07-25 12:15:28 +0530

    blog page created

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,10 @@
+import { TypingAnimation } from "@/components/magicui/typing-animation";
+
 export default function BlogPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-neutral-950 text-neutral-400">
       <p className="text-lg font-mono">
-        // Blog is under construction...
+        <TypingAnimation>Blog is under construction...</TypingAnimation>
       </p>
     </div>
   );
