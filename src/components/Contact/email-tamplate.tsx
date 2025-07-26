3d57f99ec88c5a62c7f8bf2704c725b9368e78ae
nishuldhakar <nishuldhakar123@example.com>
2025-07-26 12:17:12 +0530

    update email temp

--- src/components/Contact/email-tamplate.tsx
+++ src/components/Contact/email-tamplate.tsx
@@ -10,7 +10,9 @@ interface EmailTemplateProps {
 export function EmailTemplate({ fullName, email, phoneNo, message }: EmailTemplateProps) {
   return (
     <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px', lineHeight: '1.6' }}>
-      <h2>📬 Contact Form Submission via Nishul's Portfolio : nishul.dev</h2>
+<h2>
+  📬 Contact Form Submission via Nishul&apos;s Portfolio: <a href="https://nishul.dev" style={{ color: '#1a0dab', textDecoration: 'none' }}>nishul.dev</a>
+</h2>
       <p><strong>Full Name:</strong> {fullName}</p>
       <p><strong>Email:</strong> {email}</p>
       <p><strong>Phone Number:</strong> {phoneNo}</p>
